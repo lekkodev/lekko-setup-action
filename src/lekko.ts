83b77d3445fc5f0358d791e819be5bf894aa3cb9
Shubhit Singh <shubhit@lekko.dev>
2023-04-07 14:14:49 -0700

    Lekko setup action

---
+++ src/lekko.ts
@@ -0,0 +1,158 @@
+// Copyright 2022-2023 Lekko Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import { Octokit } from "@octokit/core";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import * as os from "os";
+import * as path from "path";
+import { Error, isError } from "./error";
+
+// versionPrefix is used in Github release names, and can
+// optionally be specified in the action's version parameter.
+const versionPrefix = "v";
+
+export async function getLekko(
+  version: string,
+  githubToken: string
+): Promise<string | Error> {
+  const binaryPath = tc.find("lekko", version, os.arch());
+  if (binaryPath !== "") {
+    core.info(`Found in cache @ ${binaryPath}`);
+    return binaryPath;
+  }
+
+  core.info(`Resolving the download URL for the current platform...`);
+  const downloadURL = await getDownloadURL(version, githubToken);
+  if (isError(downloadURL)) {
+    return downloadURL;
+  }
+
+  let cacheDir = "";
+  core.info(`Downloading lekko version "${version}" from ${downloadURL}`);
+  const downloadPath = await tc.downloadTool(downloadURL);
+  core.info(
+    `Successfully downloaded lekko version "${version}" from ${downloadURL}`
+  );
+
+  core.info("Extracting lekko...");
+  // TODO: we don't release windows platform builds currently. When we do,
+  // update this code path to extract .exe appropriately
+  const extractPath = await tc.extractTar(downloadPath);
+  core.info(`Successfully extracted lekko to ${extractPath}`);
+
+  core.info("Adding lekko to the cache...");
+  cacheDir = await tc.cacheDir(
+    path.join(extractPath, "lekko"),
+    "lekko",
+    version,
+    os.arch()
+  );
+  core.info(`Successfully cached lekko to ${cacheDir}`);
+  return cacheDir;
+}
+
+// getDownloadURL resolves Lekko's Github download URL for the
+// current architecture and platform.
+async function getDownloadURL(
+  version: string,
+  githubToken: string
+): Promise<string | Error> {
+  let architecture = "";
+  switch (os.arch()) {
+    // The available architectures can be found at:
+    // https://nodejs.org/api/process.html#process_process_arch
+    case "x64":
+      architecture = "x86_64";
+      break;
+    case "arm64":
+      architecture = "arm64";
+      break;
+    default:
+      return {
+        message: `The "${os.arch()}" architecture is not supported with a Lekko release.`,
+      };
+  }
+  let platform = "";
+  switch (os.platform()) {
+    // The available platforms can be found at:
+    // https://nodejs.org/api/process.html#process_process_platform
+    case "linux":
+      platform = "Linux";
+      break;
+    case "darwin":
+      platform = "Darwin";
+      break;
+    default:
+      return {
+        message: `The "${os.platform()}" platform is not supported with a Lekko release.`,
+      };
+  }
+  const assetName = `lekko_${platform}_${architecture}.tar.gz`;
+  const requestAgent = process.env.http_proxy
+    ? new HttpsProxyAgent(process.env.http_proxy)
+    : undefined;
+  const octokit = new Octokit({
+    auth: githubToken,
+    request: {
+      agent: requestAgent,
+    },
+  });
+  if (version === "latest") {
+    const { data: releases } = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: "lekkodev",
+        repo: "cli",
+        per_page: 1,
+      }
+    );
+    for (const asset of releases[0].assets) {
+      if (assetName === asset.name) {
+        return asset.browser_download_url;
+      }
+    }
+    return {
+      message: `Unable to find Lekko version "${version}" for platform "${platform}" and architecture "${architecture}".`,
+    };
+  }
+  const tag = releaseTagForVersion(version);
+  const { data: release } = await octokit.request(
+    "GET /repos/{owner}/{repo}/releases/tags/{tag}",
+    {
+      owner: "lekkodev",
+      repo: "cli",
+      tag: tag,
+    }
+  );
+  for (const asset of release.assets) {
+    if (assetName === asset.name) {
+      return asset.browser_download_url;
+    }
+  }
+  return {
+    message: `Unable to find Lekko version "${version}" for platform "${platform}" and architecture "${architecture}".`,
+  };
+}
+
+// releaseTagForVersion returns the release tag name based on a given version configuration.
+// Github releases include the 'v' prefix, but the user-supplied version may not. Thus, we permit
+// both versions, e.g. v0.38.0 and 0.38.0.
+function releaseTagForVersion(version: string): string {
+  if (version.indexOf(versionPrefix) === 0) {
+    return version;
+  }
+  return versionPrefix + version;
+}
---
+++ src/run.ts
@@ -0,0 +1,81 @@
+// Copyright 2022-2023 Lekko Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+import cp from "child_process";
+import * as os from "os";
+import * as path from "path";
+import { Error, isError } from "./error";
+import { getLekko } from "./lekko";
+
+export async function run(): Promise<void> {
+  try {
+    const result = await runSetup();
+    if (result !== null && isError(result)) {
+      core.setFailed(result.message);
+    }
+  } catch (error) {
+    // In case we ever fail to catch an error
+    // in the call chain, we catch the error
+    // and mark the build as a failure. The
+    // user is otherwise prone to false positives.
+    if (isError(error)) {
+      core.setFailed(error.message);
+      return;
+    }
+    core.setFailed("Internal error");
+  }
+}
+
+// runSetup runs the lekko-setup action, and returns
+// a non-empty error if it fails.
+async function runSetup(): Promise<null | Error> {
+  const version = core.getInput("version");
+  if (version === "") {
+    return {
+      message: "a version was not provided",
+    };
+  }
+  const githubToken = core.getInput("github_token");
+  if (githubToken === "") {
+    return {
+      message: "No github_token supplied, won't be able to download lekko",
+    };
+  }
+
+  core.info(`Setting up Lekko version "${version}"`);
+  const installDir = await getLekko(version, githubToken);
+  if (isError(installDir)) {
+    return installDir;
+  }
+
+  core.info("Adding lekko binary to PATH");
+  let binaryPath = "";
+  if (os.platform() === "win32") {
+    core.addPath(installDir);
+  } else {
+    core.addPath(path.join(installDir, "bin"));
+  }
+  binaryPath = await io.which("lekko", true);
+  if (binaryPath === "") {
+    return {
+      message: "lekko was not found on PATH",
+    };
+  }
+
+  core.info(`Successfully setup lekko version ${version}`);
+  core.info(cp.execSync(`${binaryPath} --version`).toString());
+  return null;
+}
